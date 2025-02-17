b3f57a4e513ce44c7ea191387cd26956a5b153e6
epommier <9117484+Epommier@users.noreply.github.com>
2025-02-17 15:53:42 +0100

    Update graph and agent workflow

--- agent.py
+++ agent.py
@@ -2,20 +2,15 @@ import os
 import uuid
 import json
 
-from typing import List, Literal, AsyncGenerator, Sequence
-from typing_extensions import Annotated
+from embeddings_tools import get_vector_store
+from typing import List, Literal, AsyncGenerator, Annotated
+from pydantic import BaseModel, Field
 from operator import add
 
-from pydantic import BaseModel, Field
-
-from langchain.schema import Document
+from langchain.schema import Document, HumanMessage
 from langchain.globals import set_verbose
-
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.messages import SystemMessage, AIMessage, BaseMessage
-
-from langchain_qdrant import QdrantVectorStore
-
+from langchain_core.messages import SystemMessage, AIMessage
 from langchain_openai import AzureChatOpenAI, AzureOpenAIEmbeddings
 
 from langgraph.checkpoint.memory import MemorySaver
@@ -42,21 +37,25 @@ class Reference(BaseModel):
             content=document.page_content,
             title=document.metadata.get('source', 'Unknown Title'),
             page=str(document.metadata.get('page', 'Unknown Page')),
-            id=document.metadata.get('_id', 'Unknown ID')
+            id=document.id
         )
     def __str__(self) -> str:
         """Returns a markdown formatted string representation of the reference"""
         return f"""
-> {self.content}
+> *{self.content}*
 >
-> <a href="{self.id}">*{self.title}*, p. {self.page}</a>
+>> **{self.title}**, p. {self.page}
 """
+
+class Evaluation(BaseModel):
+    binary_score: Literal["yes", "no"]
+    explanation: str
 
 class RagState(AgentState):
     question: str
     queries: List[str]
     references: List[Reference]
-    updates: Sequence[SystemMessage]
+    evaluations: Annotated[List[Evaluation], add]
 
 llm = AzureChatOpenAI(
     azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
@@ -67,13 +66,8 @@ llm = AzureChatOpenAI(
     timeout=None,
     max_retries=3
 )
-json_llm = AzureChatOpenAI(
-    azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
-    azure_deployment="gpt-4o",
-    openai_api_version=os.environ["AZURE_OPENAI_API_VERSION"],
-    temperature=0.0,
-    response_format={"type": "json_object"}
-)
+
+json_llm = llm.bind(response_format={"type": "json_object"})
 
 def generate_queries(state: RagState):
     question = state["question"]
@@ -81,24 +75,22 @@ def generate_queries(state: RagState):
         ("system", open("prompts\\queries_prompt.txt", "r").read()),
         ("user", "{question}")])
 
-    response = json_llm.invoke(prompt.format(question=question))
+    response = json_llm.invoke(prompt.format(question=question, language=os.environ["LANGUAGE"]))
     parsed_response = json.loads(response.content)
     return {
         "queries": parsed_response["queries"],
-        "updates": [
-            SystemMessage(content="🔍 Generating search queries for your question..."),
-            SystemMessage(content="📚 Searching through the document collection...")
-        ]
+        "updates": [SystemMessage(content="📚 Searching through the document collection...")],
+        "messages": [HumanMessage(content=question)]
     }
 
 def retrieve_references(state: RagState):
     queries = state["queries"]
-    retriever = QdrantVectorStore.from_existing_collection(
-        collection_name=os.environ["QDRANT_COLLECTION"],
-        embedding=embeddings,
-        content_payload_key="text",
-        url=os.environ.get("QDRANT_URL"),
-        api_key=os.environ.get("QDRANT_API_KEY")).as_retriever(k=5)
+
+    vector_store = get_vector_store(os.environ["VECTOR_COLLECTION"], embeddings)
+    retriever = vector_store.as_retriever(
+        search_type="similarity",
+        search_kwargs={"k": 10}
+    )
 
     docs = [doc for query in queries for doc in retriever.invoke(query)]
     references = [Reference.from_document(doc) for doc in docs]
@@ -107,8 +99,7 @@ def retrieve_references(state: RagState):
     return {
         "references": list(unique_refs_dict.values()),
         "updates": [
-            SystemMessage(content=f"📎 Found {len(unique_refs_dict)} relevant references"),
-            SystemMessage(content="🤔 Generating answer based on the references...")
+            SystemMessage(content=f"📎 Found {len(unique_refs_dict)} relevant references."),
         ]
     }
 
@@ -117,30 +108,68 @@ def generate_answer(state: RagState):
     references_txt = "\n".join([str(ref) for ref in state["references"]])
     prompt = ChatPromptTemplate.from_messages([
         ("system", open("prompts\\answer_prompt.txt", "r").read()),
-        ("user", "{question}")])
+        ("user", "QUESTION: {question}\n\nREFERENCES: {references}")])
 
-    response = llm.invoke(prompt.format(question=query, references=references_txt))
+    response = llm.invoke(
+        prompt.format(
+            language=os.environ["LANGUAGE"],
+            question=query,
+            references=references_txt
+        )
+    )
     
     return {
-        "messages": [            
-            AIMessage(content=response.content)
-        ]
+        "messages": [AIMessage(content=response.content)]
     }
 
-def evaluate_answer(state: RagState) -> Literal["end", "loop"]:
+def evaluate_answer(state: RagState):
     answer = state["messages"][-1].content
     references = "\n".join([str(ref) for ref in state["references"]])
     prompt = ChatPromptTemplate.from_messages([
         ("system", open("prompts\\grade_prompt.txt", "r").read()),
-        ("user", "FACTS: {references}\n\nANSWER: {answer}")])
+        ("user", "FACTS: {references}\n\nANSWER: {answer}")]
+    )
     
     result = json_llm.invoke(prompt.format(references=references, answer=answer))
-    score = json.loads(result.content)["binary_score"]
+    evaluation = Evaluation.model_validate_json(result.content)
 
-    if score == "yes":
+    return {
+        "evaluations": [evaluation]
+    }
+
+def evaluate_edge(state: RagState) -> Literal["end", "loop"]:
+    evaluation = state["evaluations"][-1]
+    if evaluation.binary_score == "yes":
         return "end"
     else:
         return "loop"
+
+def correct_answer(state: RagState):
+    answer = state["messages"][-1].content
+    evaluation = state["evaluations"][-1]
+    question = state["question"]
+    references = "\n".join([str(ref) for ref in state["references"]])
+
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", open("prompts\\grade_prompt.txt", "r").read()),
+        ("user", """QUESTION: {question}\n\n
+            REFERENCES: {references}\n\n
+            Improve the below answer, taking into account the professor's remarks.\n\n
+            ANSWER: {answer}\n\n
+            PROFESSOR REMARKS:{evaluation}"""
+        )
+    ])
+
+    response = llm.invoke(prompt.format(
+        references=references,
+        question=question,
+        answer=answer,
+        evaluation=evaluation.justification)
+    )
+
+    return {
+        "messages": [AIMessage(content=response.content)]
+    }
 
 def construct_agent_graph() -> CompiledStateGraph:
     checkpointer = MemorySaver()
@@ -149,16 +178,20 @@ def construct_agent_graph() -> CompiledStateGraph:
     workflow.add_node("generatequeries", generate_queries);
     workflow.add_node("retrievereferences", retrieve_references);
     workflow.add_node("generateanswer", generate_answer);
+    workflow.add_node("evaluateanswer", evaluate_answer);
+    workflow.add_node("correctanswer", correct_answer);
 
     workflow.set_entry_point("generatequeries")
     workflow.add_edge("generatequeries", "retrievereferences")
     workflow.add_edge("retrievereferences", "generateanswer")
+    workflow.add_edge("generateanswer", "evaluateanswer")
+    workflow.add_edge("correctanswer", "evaluateanswer")
     workflow.add_conditional_edges(
-        "generateanswer",
-        evaluate_answer,
+        "evaluateanswer",
+        evaluate_edge,
         { 
             "end": END,
-            "loop": "generateanswer"
+            "loop": "correctanswer"
         }
     )
 
@@ -183,8 +216,7 @@ async def arun_agent(question: str) -> AsyncGenerator[dict, None]:
         queries=[],
         references=[]
     )
-    
-    # Use astream to get intermediate results
+
     async for _ in graph.astream(initial_state, config=config):
         data = graph.get_state(config=config).values
         yield data
